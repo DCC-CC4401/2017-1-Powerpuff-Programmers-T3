c0c8634ceb9e91ba87670ffca648b6270f21bff9
Argorthas <warcraft123>
2017-05-26 18:58:57 -0400

    Actualización del modelo, con largos estandares

--- usnacks/app/models.py
+++ usnacks/app/models.py
@@ -2,26 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Vendedor(models.Model):
-    VEN_USERNAME = models.CharField(max_length = 3º0)
-    VEN_PASSWORD = models.CharField(max_length = 20)
-    VEN_TIPO = models.CharField(max_lenghth = 20)
-    VEN_PAGO = models.CharField(max_length = 60)
+    VEN_USERNAME = models.CharField(max_length = 255)
+    VEN_PASSWORD = models.CharField(max_length = 255)
+    VEN_TIPO = models.CharField(max_lenghth = 255)
+    VEN_PAGO = models.CharField(max_length = 255)
     VEN_FOTO = models.ImageField('Foto de perfil')
     VEN_HORARIOA = models.TimeField('Horario de apertura')
     VEN_HORARIOC = models.TimeField('Horario de cierre')
     VEN_ACTIVO = models.BooleanField()
 
 class Alumno(models.Model):
-    ALU_USERNAME = models.CharField(max_length = 20)
-    ALU_PASSWORD = models.CharField(max_length = 20)
+    ALU_USERNAME = models.CharField(max_length = 255)
+    ALU_PASSWORD = models.CharField(max_length = 255)
 
 class Producto(models.Model):
-    PRO_NOMBRE = models.CharField(max_length = 20)
-    PRO_DESCRIPCION = models.CharField(max_length = 200)
+    PRO_NOMBRE = models.CharField(max_length = 255)
+    PRO_DESCRIPCION = models.CharField(max_length = 255)
     PRO_FOTO = models.ImageField('Foto de producto')
     PRO_PRECIO = models.IntegerField()
     PRO_STOCK = models.IntegerField()
-    PRO_CATEGORIA = models.CharField(max_length = 60)
+    PRO_CATEGORIA = models.CharField(max_length = 255)
     PRO_IDVENDEDOR = models.ForeignKey(Vendedor, on_delete = models.CASCADE)
 
 class Favoritos(models.Model):
