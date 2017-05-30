0de16a7e6cf74bcca54602ac0cab013b3f632b34
Juan José Álvarez Tapia <jjatjenkins1@gmail.com>
2017-05-30 04:53:03 -0400

    update

--- usnacks/app/models.py
+++ usnacks/app/models.py
@@ -1,34 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=60)
+    password = models.CharField(max_length=60)
+    tipo = models.CharField(max_length=60)
+    foto = models.ImageField('Foto de perfil')
+
+    def __str__(self):
+        return self.username
+
+class MetodosDePago(models.Model):
+    name = models.CharField(max_length = 30)
+
+class Categoria(models.Model):
+    name = models.CharField(max_length = 30)
+
+class Localizacion(models.Model):
+    posx = models.FloatField()
+    posy = models.FloatField()
+
 class Vendedor(models.Model):
-    VEN_USERNAME = models.CharField(max_length = 255)
-    VEN_PASSWORD = models.CharField(max_length = 255)
-    VEN_TIPO = models.CharField(max_lenghth = 255)
-    VEN_PAGO = models.CharField(max_length = 255)
-    VEN_FOTO = models.ImageField('Foto de perfil')
-    VEN_HORARIOA = models.TimeField('Horario de apertura')
-    VEN_HORARIOC = models.TimeField('Horario de cierre')
-    VEN_ACTIVO = models.BooleanField()
+    title = models.CharField(max_length=10)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pago = models.ManyToManyField(MetodosDePago)
+    activo = models.BooleanField()
+    hapertura = models.TimeField('Horario de apertura', null=True)
+    hcierre = models.TimeField('Horario de cierre', null=True)
+    localizacion = models.OneToOneField(Localizacion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 class Alumno(models.Model):
-    ALU_USERNAME = models.CharField(max_length = 255)
-    ALU_PASSWORD = models.CharField(max_length = 255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class Producto(models.Model):
-    PRO_NOMBRE = models.CharField(max_length = 255)
-    PRO_DESCRIPCION = models.CharField(max_length = 255)
-    PRO_FOTO = models.ImageField('Foto de producto')
-    PRO_PRECIO = models.IntegerField()
-    PRO_STOCK = models.IntegerField()
-    PRO_CATEGORIA = models.CharField(max_length = 255)
-    PRO_IDVENDEDOR = models.ForeignKey(Vendedor, on_delete = models.CASCADE)
+    title = models.CharField(max_length=10)
+    nombre= models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=200)
+    foto = models.ImageField('Foto de producto')
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    categoria = models.ManyToManyField(Categoria)
+    vendedorId = models.ForeignKey(Vendedor, on_delete=models.CASCADE)
 
 class Favoritos(models.Model):
-    FAV_IDALUMNO = models.ForeignKey(Alumno, on_delete = models.CASCADE)
-    FAV_IDVENDEDOR = models.ForeignKey(Vendedor, on_delete = models.CASCADE)
+    alumnoId = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    vendedorId = models.ForeignKey(Vendedor, on_delete=models.CASCADE)
 
 class Historial(models.Model):
-    HIS_IDPRODUCTO = models.ForeignKey(Producto, on_delete = models.CASCADE)
-    HIS_FECHA = models.TimeField('fecha')
-    HIS_STOCK = models.IntegerField()
+    ProductoId = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    fecha = models.TimeField('fecha')
+    Hstock = models.IntegerField()
+
